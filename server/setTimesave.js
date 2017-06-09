8df9d849d56b2ce5b620de352e9cc704b941899b
cky <576779975@qq.com>
2017-06-09 17:34:37 +0800

    add:延时间隔爬取，减少验证码出现次数

--- server/setTimesave.js
+++ server/setTimesave.js
@@ -4,71 +4,65 @@ var schedule = require('node-schedule');
 var PostList = AV.Object.extend('PostList');
 
 function scheduleRecurrenceRule(){
-    var rule  = new schedule.RecurrenceRule();  
+    let runTime = 0;
+    let rule  = new schedule.RecurrenceRule();  
     //在指定的小时抓取一次
     rule.hour =[1,7,11,16,20];
     rule.minute = 0;
+    let wxIdList = ['JavaScriptcn','cjscwe_2015','FeZaoDuKe','FrontendMagazine','FrontDev'];
+    
     schedule.scheduleJob(rule, function(){
-        let wxIdList = ['JavaScriptcn','cjscwe_2015','FeZaoDuKe','FrontendMagazine','FrontDev'];
         console.log('定时任务开始执行:' + new Date());
-        getWxPostAndSave(wxIdList).then(rs=>{
-            if(rs.success){
-                console.log(rs.msg);
+        //为了防止多次出现验证码，延时10s分别拉取
+        let interval = setInterval(function(){
+            if(runTime === wxIdList.length -1){
+                clearInterval(interval);
             }
-        }).catch(err=>{
-            console.error('定时获取文章失败：'+ err + new Date());
-        });
+            getWxPostAndSave(wxIdList[runTime]).then(rs=>{
+                console.log(`开始爬取${wxIdList[runTime]}的文章`);
+                if(rs.success){
+                    console.log(rs.msg);
+                }
+            }).catch(err=>{
+                console.error('定时获取文章失败：'+ err + new Date());
+            });
+
+            runTime += 1;
+            
+        }, 10000);
     });
    
 }
 
 //爬取微信文章并存入leancloud
-function getWxPostAndSave(wxIdList){
+function getWxPostAndSave(wxId){
     return new Promise((resolve,reject)=>{
-        let searchWxUrlList = [];
-        let wxIdLen = wxIdList.length;
-        let wxPostResultList = [];
-        //批量获取微信路径
-        for(let i = 0;i < wxIdLen; i++){
-            searchWxUrlList.push(Ut.getWxUrl(wxIdList[i]));
-        }
-        //批量获取微信文章
-        Promise.all(searchWxUrlList).then(rs=>{
-            let searchWxPostList = [];
-            for(let i = 0;i < rs.length;i++){
-                if(rs[i].success){
-                    searchWxPostList.push(Ut.getWxPostInfo(rs[i]));
-                }
+        let wxPostResult= null;
+        //获取微信文章
+        Ut.getWxUrl(wxId).then(rs=>{
+            if(rs.success){
+                return Ut.getWxPostInfo(rs);
             }
-            return Promise.all(searchWxPostList);
         }).then(rs=>{
             //查询存储的数据id
-            let queryIdList = [];
-            wxPostResultList = rs;
-            for(let i = 0;i < wxIdLen; i++){
-                let query = new AV.Query('PostList');
-                query.equalTo('wxId', wxIdList[i]);
-                queryIdList.push(query.find());
-            }
-            return Promise.all(queryIdList);
+            let query = new AV.Query('PostList');
+            query.equalTo('wxId', wxId);
+            wxPostResult = rs;
+            return query.find();
         }).then(rs=>{
             //根据查询到的Id进行数据更新或者新建
-            let saveWxPostList = [];
-            for(let i = 0;i < wxIdLen; i++){
-                let qeuryId = rs[i][0] ? rs[i][0].id :'';
-                let post = AV.Object.createWithoutData('PostList', qeuryId);
-                post.set('postList', JSON.stringify(wxPostResultList[i]));
-                post.set('wxId',wxIdList[i]);
-                saveWxPostList.push(post.save());
-            }
-            return Promise.all(saveWxPostList);
+            let qeuryId = rs[0] ? rs[0].id :'';
+            let post = AV.Object.createWithoutData('PostList', qeuryId);
+            post.set('postList', JSON.stringify(wxPostResult) );
+            post.set('wxId',wxId);
+            return post.save();
         }).then(rs=>{
             resolve({success:true,msg:'保存成功'});
         }).catch(err=>{
             console.error(err);
             reject(err);
         });
-    })
+    });
 }
 
 module.exports = scheduleRecurrenceRule;
